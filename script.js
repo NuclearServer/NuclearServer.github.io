df12ca76de51834872a42f2892c273372c6fceed
mif1k <104626075+mif1k@users.noreply.github.com>
2023-11-05 20:49:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-document.getElementById('button1').addEventListener('click', () => {
-  document.getElementById('audio1').play()
-})
 
-document.getElementById('button1').addEventListener('dblclick', () => {
-  document.getElementById('audio1').pause()
-})
-
-
-document.getElementById('button2').onclick = function() {
-  document.getElementById('div1').hidden = true;
-}
