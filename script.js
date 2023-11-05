10c6c91b46b38d5e7c8d4ad05303a7c60a01b3a6
mif1k <104626075+mif1k@users.noreply.github.com>
2023-11-05 16:05:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('button').addEventListener('click', () => {
+  document.querySelector('audio').play()
+})
+
+document.querySelector('button').addEventListener('dblclick', () => {
+  document.querySelector('audio').pause()
+})
